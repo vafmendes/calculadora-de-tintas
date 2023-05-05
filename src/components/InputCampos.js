d6de8a5d299f8368d99f7a65673912fd6079313c
vafmendes <vmendes667@gmail.com>
2023-05-05 10:12:13 -0300

    Realizado atualização de condições e adicionado comentários para explicação de variáveis

--- src/components/InputCampos.js
+++ src/components/InputCampos.js
@@ -17,89 +17,67 @@ function InputCampos() {
       janelas: parseInt(prompt(`Informe a quantidade de janelas da parede ${paredes.length + 1}:`)),
     };
 
-    if ((!isNaN(novaParede.largura) && !isNaN(novaParede.altura))  && (!isNaN(novaParede.portas) && !isNaN(novaParede.janelas)) ) { 
-      if((novaParede.portas > 0) && (novaParede.altura <= 2.20) ){
+    if ((!isNaN(novaParede.largura) && !isNaN(novaParede.altura)) && (!isNaN(novaParede.portas) && !isNaN(novaParede.janelas))) {
+      if ((novaParede.portas > 0) && (novaParede.altura <= 2.20)) {
         alert("A altura das paredes que possuem porta deve possuir pelo menos 30 centímetros de distância com relação a altura da porta");
         return;
-      }else if((novaParede.altura * novaParede.largura < 1) || (novaParede.altura * novaParede.largura > 50)){
+      } else if ((novaParede.altura * novaParede.largura < 1) || (novaParede.altura * novaParede.largura > 50)) {
         alert("Nenhuma parede pode ter menos de 1 metro quadrado nem mais de 50 metros quadrados");
-        setParedes([]);
-      }else{
+        return;
+      } else {
         setParedes([...paredes, novaParede]);
         alert(`Parede ${paredes.length + 1} adicionada`);
       }
     } else {
-      alert("Os valores não podem ser nulos");
+      alert("Não deve haver valores nulos");
       setParedes([]);
     }
 
     if (paredes.length === 3) {
-          setCompleto(true);
+      setCompleto(true);
     }
 
   }
 
+  /*Quantidade de portas*/
   const quantPortas = paredes.map((parede) => Number(parede.portas));
- 
+
+  /*Quantidade de janelas*/
   const quantJanelas = paredes.map((parede) => Number(parede.janelas));
-  
-  const areaPortas = quantPortas.reduce((porta, cur) =>  porta + cur * 2.4, 0).toFixed(2);
-  
-  const areaJanelas = quantJanelas.reduce((janela, cur) => janela + cur * 1.52, 1).toFixed(2);
 
-  const quantLargAlt = paredes.map((parede) =>{ return Number(parede.largura * parede.altura)} );
+  /*Área pré-definida pela regra de negócio de cada porta */
+  const areaPortas = quantPortas.reduce((porta, cur) => porta + cur * 2.4, 0).toFixed(2);
+  console.log(areaPortas);
 
-  const areaTotalParedes = quantLargAlt.reduce((porta, cur) =>  porta + cur, 0);
+  /*Área pré-definida pela regra de negócio de cada janela */
+  const areaJanelas = quantJanelas.reduce((janela, cur) => janela + cur * 1.52, 0).toFixed(2);
+  console.log(areaJanelas);
 
-  if(areaPortas + areaJanelas > areaTotalParedes / 2){
+  /*Área em m² de cada parede inserida pelo usuário*/
+  const quantLargAlt = paredes.map((parede) => { return Number(parede.largura * parede.altura) });
+
+  /*Soma de todas as áreas das parede */
+  const areaTotalParedes = quantLargAlt.reduce((porta, cur) => porta + cur, 0);
+
+  if (areaPortas + areaJanelas > (areaTotalParedes / 2)) {
     alert("O total de área das portas e janelas não poderá ocupar 50% da área de parede");
     setCompleto(false);
     setParedes([]);
 
   }
 
-  const totalPortas = quantPortas.reduce((porta, cur) =>  porta + cur, 0);
-  
+  /*Soma de todas as portas */
+  const totalPortas = quantPortas.reduce((porta, cur) => porta + cur, 0);
+  /*Soma de todas as paredes */
   const totalJanelas = quantJanelas.reduce((janela, cur) => janela + cur, 0);
-  
 
-  const imprimirResultado = ()=>{
-      let descontaArea = areaTotalParedes - areaJanelas - areaPortas;
-      let litrosTinta = Math.ceil(descontaArea / 5);
-  
-      if (litrosTinta <= 0) {
-        return (<p>Nenhuma tinta é necessária.</p>);
-      }
-  
-      if (litrosTinta <= 0.5) {
-        return (<p>Compre uma lata de 0,5 litros de tinta.</p>);
-      }
-  
-      if (litrosTinta <= 2.5) {
-        return (<p>Compre uma lata de 2,5 litros de tinta.</p>);
-      }
-  
-      if (litrosTinta <= 3.6) {
-        return (<p>Compre uma lata de 3,6 litros de tinta.</p>);
-      }
-  
-      let latasNecessarias = Math.ceil(litrosTinta / 18);
-      console.log(latasNecessarias);
-        if(latasNecessarias === 1){
-          return (<p> Compre {latasNecessarias} lata de 18 litros de tinta.</p>);
-        }else{
-          return (<p> Compre {latasNecessarias} latas de 18 litros de tinta.</p>);
-        }
-
-
-  }
 
   function imprimirMedidas() {
     return (
       <div style={{ margin: "10px 30px" }}>
         {paredes.map((parede, index) => (
-          <p key={index} style={{marginBottom:"10px", paddingRight:"35px"}}>
-            Parede {index + 1}: Largura - {parede.largura.toFixed(2)} | Altura - {parede.altura.toFixed(2)} | Portas: {parede.portas} | Janelas: {parede.janelas} | Área: Área: {(parede.largura * parede.altura).length >= 5  ? (parede.largura * parede.altura).toFixed(2) : parede.largura * parede.altura}m²
+          <p key={index} style={{ marginBottom: "10px", paddingRight: "35px" }}>
+            Parede {index + 1}: Largura - {parede.largura.toFixed(2)} | Altura - {parede.altura.toFixed(2)} | Portas: {parede.portas} | Janelas: {parede.janelas} | Área: {((parede.largura * parede.altura).toString()).length >= 5 ? (parede.largura * parede.altura).toFixed(2) : parede.largura * parede.altura}m²
           </p>
         ))}
         <div style={{ marginTop: "10px" }}>
@@ -110,7 +88,30 @@ function InputCampos() {
     );
   }
 
-  function refazerCalculo(){
+  const imprimirResultado = () => {
+    let descontaArea = areaTotalParedes - areaJanelas - areaPortas;
+    let litrosTinta = Math.ceil(descontaArea / 5);
+
+    if (litrosTinta <= 0) {
+        return (<p>Nenhuma tinta é necessária.</p>);
+    } else if (litrosTinta <= 0.5) {
+        return (<p>Compre uma lata de 0,5 litros de tinta.</p>);
+    } else if (litrosTinta <= 2.5) {
+        return (<p>Compre uma lata de 2,5 litros de tinta.</p>);
+    } else if (litrosTinta <= 3.6) {
+        return (<p>Compre uma lata de 3,6 litros de tinta.</p>);
+    } else {
+        let latasNecessarias = Math.ceil(litrosTinta / 18);
+          if (latasNecessarias === 1) {
+            return (<p> Compre {latasNecessarias} lata de 18 litros de tinta.</p>);
+          } else {
+            return (<p> Compre {latasNecessarias} latas de 18 litros de tinta.</p>);
+          }
+    }
+
+  }
+
+  function refazerCalculo() {
     setCompleto(false);
     setParedes([]);
     return adicionarValores;
@@ -123,8 +124,8 @@ function InputCampos() {
       <p>Clique no botão abaixo e informe a medida de cada parede de sua sala para calcular a quantidade de tinta necessária para pintar. Incluindo quantidade de janelas e portas que cada parede possui.</p>
       {(!completo) && paredes[0] && <h5>Para adicionar outra parede clique novamente</h5>}
       {!completo && (<button onClick={adicionarValores}>+ Adicionar parede</button>)}
-      {completo &&<PrintCampos valor={imprimirMedidas()}/>}
-      {completo &&<Resultado enviarValor= {imprimirResultado()} />}
+      {completo && <PrintCampos valor={imprimirMedidas()} />}
+      {completo && <Resultado enviarValor={imprimirResultado()} />}
       {completo && <h5>Caso deseje fazer um novo cálculo, clique em 'Reiniciar'</h5>}
       {completo && <button onClick={refazerCalculo}>Reiniciar</button>}
     </div>
