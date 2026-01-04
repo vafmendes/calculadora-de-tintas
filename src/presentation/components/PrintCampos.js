17a7ec5423b4afe5c2035f2470152a5594e33a08
vafmendes <vmendes667@gmail.com>
2026-01-04 07:41:21 -0300

    refactor: exclusão dos arquivos da versão anterior do projeto

--- src/presentation/components/PrintCampos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface PrintCamposProps {
-  valor: React.ReactNode;
-}
-
-export function PrintCampos({ valor }: PrintCamposProps): React.ReactElement {
-  return <div>{valor}</div>;
-}
--- src/presentation/components/Resultado.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface ResultadoProps {
-  enviarValor: React.ReactNode;
-}
-
-export function Resultado({ enviarValor }: ResultadoProps): React.ReactElement {
-  return <div>Resultado: {enviarValor}</div>;
-}
